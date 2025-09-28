74bda0777026f126817d4f76476ca00dc2eb5dd4
Juan Pereira Robi <juanpereirarobi@gmail.com>
2025-09-27 21:21:51 -0300

    Atualização

--- app.rb
+++ app.rb
@@ -22,12 +22,21 @@ post '/contact' do
         }
     end
 
-    Mail.deliver do
-        to email
-        from ENV['EMAIL_APP']
-        subject 'Novo contato'
-        body message
-    end
+    begin
+        Mail.deliver do
+            to ENV['EMAIL_APP']
+            from email
+            subject 'Novo contato'
+            body message
+        end
+
+        erb :aviso, locals: { message: 'Mensagem enviada com sucesso!' }
+    rescue => e
+        erb :aviso, locals: { message: "Erro ao enviar: #{e.message}" }
+    end 
+
+    puts "EMAIL_APP: #{ENV['EMAIL_APP']}"
+    puts "PASSWORD_APP: #{ENV['PASSWORD_APP']}"
 
     erb :aviso, locals: { message: 'Mensagem enviada com sucesso!' }
 end
